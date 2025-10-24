8dde1840b098c10b3d8d0539cb783c299ff78f52
nnikolajj <greger.nikolaj@gmail.com>
2025-10-24 16:58:46 +0200

    adds uebung 06

---
+++ src/Testing/testereter.java
@@ -0,0 +1,8 @@
+package Testing;
+
+public class testereter {
+    static void main() {
+        String text1 = "Sommerzeit";
+        System.out.println(text1.replaceAll("[^aeiou]{2}", "8"));
+    }
+}
